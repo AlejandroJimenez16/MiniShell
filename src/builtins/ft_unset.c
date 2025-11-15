35a09d8efe2ec570c576c786b77bf757945a93ec
AlejandroJimenez16 <alejanjiga@gmail.com>
2025-11-15 19:34:10 +0100

    unset hecho :)

--- src/builtins/ft_unset.c
+++ src/builtins/ft_unset.c
@@ -6,7 +6,7 @@
 /*   By: alejandj <alejandj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/13 19:40:22 by alejandj          #+#    #+#             */
-/*   Updated: 2025/11/14 18:39:04 by alejandj         ###   ########.fr       */
+/*   Updated: 2025/11/15 19:31:01 by alejandj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,68 @@
 int	find_var_env(char **env, char *var)
 {
 	int	i;
+	int	len;
 
+	len = ft_strlen(var);
 	i = 0;
 	while (env[i])
 	{
-		if (ft_strncmp(env[i], var, ft_strlen(var)) == 0)
+		if (ft_strncmp(env[i], var, ft_strlen(var)) == 0
+			&& (env[i][len] == '=' || env[i][len] == '\0'))
 			return (1);
 		i++;
 	}
 	return (0);
+}
+
+char	**init_new_env(char **env)
+{
+	int		num_vars;
+	char	**new_env;
+
+	num_vars = 0;
+	while (env[num_vars])
+		num_vars++;
+	new_env = malloc((num_vars) * sizeof(char *));
+	if (!new_env)
+		return (NULL);
+	return (new_env);
+}
+
+char	**build_new_env(char **env, char *var)
+{
+	char	**new_env;
+	int		i;
+	int		j;
+	int		len;
+
+	new_env = init_new_env(env);
+	len = ft_strlen(var);
+	i = 0;
+	j = 0;
+	while (env[i])
+	{
+		if (!(ft_strncmp(env[i], var, len) == 0
+				&& (env[i][len] == '=' || env[i][len] == '\0')))
+		{
+			new_env[j] = ft_strdup(env[i]);
+			j++;
+		}
+		i++;
+	}
+	new_env[j] = NULL;
+	return (new_env);
+}
+
+void	unset_var(t_mini *mini, char *var)
+{
+	char	**old_env;
+
+	if (!find_var_env(mini->env, var))
+		return ;
+	old_env = mini->env;
+	mini->env = build_new_env(mini->env, var);
+	ft_free_wa(old_env);
 }
 
 void	ft_unset(t_mini *mini)
@@ -40,15 +93,13 @@ void	ft_unset(t_mini *mini)
 	i = 1;
 	while (arr[i])
 	{
-		if (arr[i][0] == '"' || arr[i][0] == '\'')
-			var = remove_quotes(arr[i], ft_strlen(arr[i]));
-		else
-			var = ft_strdup(arr[i]);
-		if (find_var_env(mini->env, var))
+		var = remove_quotes(arr[i], ft_strlen(arr[i]));
+		if (var)
 		{
-			printf("LA ENCONTRE\n");
+			if (!ft_strchr(var, '='))
+				unset_var(mini, var);
+			free(var);
 		}
-		free(var);
 		i++;
 	}
 	ft_free_wa(arr);
