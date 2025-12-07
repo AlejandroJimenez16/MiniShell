0c6678a894d8ce71e837db4951824f799d6ab269
Alex <alexmihaiconstantiniu@gmail.com>
2025-12-07 21:43:55 +0100

    Signals init añadido, falta implementarlo
    Se debe implementar la inicialización de señales al principio del programa y cada vez que se crea un proceso hijo

---
+++ src/signals/signals.c
@@ -0,0 +1,42 @@
+#include "../../includes/mini.h"
+
+/*
+** Global variable that stores exit code when recieving a signal
+*/
+int g_sig_status = 0;
+
+/*
+** Handler for SIGINT (Ctrl+C) in prompt.
+** 1. Prints a new line.
+** 2. Tells readline we are in a new line.
+** 3. Replace the actual buffer with an empty one.
+** 4. Prints the prompt again.
+*/
+void	handle_sigint(int sig)
+{
+	(void)sig;
+	g_sig_status = 130;
+	write(1, "\n", 1);
+	rl_on_new_line();
+	rl_replace_line("", 0);
+	rl_redisplay();
+}
+
+/*
+** Initialice signals for interaction mode (prompt)
+*/
+void	init_signals(void)
+{
+	signal(SIGINT, handle_sigint);
+	signal(SIGQUIT, SIG_IGN); // Ignore Ctrl+\ in prompt
+}
+
+/*
+** Configure signals for child processes.
+** Establish default behavior, so comands like cat can be closed with Ctrl+C.
+*/
+void	setup_child_signals(void)
+{
+	signal(SIGINT, SIG_DFL);
+	signal(SIGQUIT, SIG_DFL);
+}
